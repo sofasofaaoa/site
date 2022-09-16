bb3969fea31de200ade7d461da14f7fdb0771bad
sofasofaaoa <ladyrobertus3@gmail.com>
2022-09-16 16:57:38 +0700

    16.09

--- LocalLibrary/catalog/views.py
+++ LocalLibrary/catalog/views.py
@@ -35,6 +35,16 @@ from django.views import generic
 
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 10
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
+
