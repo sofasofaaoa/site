226a25a07099ec763c491fc12a2729fff2d960e5
sofasofaaoao <ladyrobertus3@gmail.com>
2022-10-26 22:21:32 +0700

    26.10

--- LocalLibrary/LocalLibrary/settings.py
+++ LocalLibrary/LocalLibrary/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@-#cfrjvl3j6eati^t*m-s$1w3pjw@^mzm&ou$a3$cndgod^)p'
 
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 LOGIN_REDIRECT_URL = '/'
